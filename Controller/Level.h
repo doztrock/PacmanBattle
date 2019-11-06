fc5b169cc7fe282aad9cdd1181f1fe5d59c2f1b3
Ivan Botero <ivan.botero@protonmail.ch>
2019-11-06 17:51:53 -0500

    Se organizan archivos junto a comentarios

---
+++ Controller/Level.h
@@ -0,0 +1,32 @@
+/**
+ ** Level.h
+ **
+ ** Contenido:  Estructura designada para almacenar los valores
+ **             de los niveles obtenidos por parte del control.
+ **
+ **/
+
+#ifndef LEVEL_H
+#define LEVEL_H 1
+
+#include <stdint.h>
+
+/**
+ * NIVELES
+ */
+
+/**
+ * Definicion: Estructura de nivel 
+ */
+typedef struct __level {
+  uint8_t LEFT_HIGH;
+  uint8_t LEFT_LOW;
+  uint8_t UP_HIGH;
+  uint8_t UP_LOW;
+  uint8_t DOWN_HIGH;
+  uint8_t DOWN_LOW;
+  uint8_t RIGHT_HIGH;
+  uint8_t RIGHT_LOW;
+} Level;
+
+#endif
--- Controller/PacmanBattle.h
+++ Controller/PacmanBattle.h
@@ -9,51 +9,40 @@
 #ifndef PACMANBATTLE_H
 #define PACMANBATTLE_H 1
 
+/**
+ * SERIAL
+ */
 
-/* Velocidad de transmision */
+/**
+ * Definicion: Velocidad de transmision
+ */
 #define BAUD_RATE 9600
 
 
-/* Pines: Control #1 */
+/**
+ * ARDUINO
+ */
+
+/** 
+ * Definicion: RX/TX Control #1
+ */
 #define RX_PIN_CONTROL_1  A0
 #define TX_PIN_CONTROL_1  10
 
-
-/* Pines: Control #2 */
+/** 
+ * Definicion: RX/TX Control #2
+ */
 #define RX_PIN_CONTROL_2  A2
 #define TX_PIN_CONTROL_2  9
 
 
-/** Niveles */
+/**
+ * INSTRUCCIONES 
+ */
 
-// LEFT
-#define BUTTON_LEFT_HIGH  187
-#define BUTTON_LEFT_LOW   163
-
-// UP
-#define BUTTON_UP_HIGH    127
-#define BUTTON_UP_LOW     110
-
-// DOWN
-#define BUTTON_DOWN_HIGH  85
-#define BUTTON_DOWN_LOW   70
-
-// RIGHT
-#define BUTTON_RIGHT_HIGH 65
-#define BUTTON_RIGHT_LOW  67
-
-// SHOOT
-#define BUTTON_SHOOT_HIGH 181
-#define BUTTON_SHOOT_LOW  170
-
-// SWITCH
-#define BUTTON_SWITCH_HIGH 206
-#define BUTTON_SWITCH_LOW  201
-
-
-/** Instrucciones */
-
-// PRE-LECTURA
+/**
+ * Definicion: Deteccion
+ */
 #define NOTHING   '0'
 #define LEFT      '1'
 #define UP        '2'
@@ -62,28 +51,26 @@
 #define SHOOT     '5'
 #define SWITCH    '6'
 
-// LECTURA
+/** 
+ * Definicion: Lectura
+ */
 #define LEFT_CONTROL_1    '7'
 #define LEFT_CONTROL_2    '8'
-
 #define UP_CONTROL_1      '9'
 #define UP_CONTROL_2      'A'
-
 #define DOWN_CONTROL_1    'B'
 #define DOWN_CONTROL_2    'C'
-
 #define RIGHT_CONTROL_1   'D'
 #define RIGHT_CONTROL_2   'E'
-
 #define SHOOT_CONTROL_1   'F'
 #define SHOOT_CONTROL_2   'G'
-
 #define SWITCH_CONTROL_1  'H'
 #define SWITCH_CONTROL_2  'I'
 
-// ESCRITURA
+/**
+ * Definicion: Escritura 
+ */
 #define VIBRATE_CONTROL_1 'J'
 #define VIBRATE_CONTROL_2 'K'
-
 
 #endif
